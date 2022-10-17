6fef1dee6977c612d693822541869cb5a23a64bd
unknown <bermobiz32@gmail.com>
2022-10-17 11:30:57 +0200

    Début du TP 2

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,10 +3,13 @@ package personnages;
 public class Village {
 	private Chef chef;
 	private String nom;
-	public Village(Chef chef, String nom) {
+	private String[] villageois;
+	private int nbVillageois=0;
+	public Village(String nom, int nbVillageoisMaximum) {
 		super();
-		this.chef = chef;
+		//this.chef = chef;
 		this.nom = nom;
+		villageois=new String[nbVillageoisMaximum];
 	}
 	public Chef getChef() {
 		return chef;
@@ -19,5 +22,17 @@ public class Village {
 	}
 	public void setNom(String nom) {
 		this.nom = nom;
+	}
+	public void ajouterHabitant(String nom) {
+		villageois[nbVillageois]=nom;
+		nbVillageois+=1;
+	}
+	public void trouverHabitant(int nmVillageois) {
+	}
+	public static void main() {
+		Village village;
+		village= new Village("Village des Irréductibles",30);
+		//Gaulois gaulois= village.trouverHabitant(30);
+		// L'exception du point précédent est Type mismatch:cannot convert from void to Gaulois. On obtient cette exception  
 	}
 }
